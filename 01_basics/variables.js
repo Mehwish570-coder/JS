804e1f19e426bc00bbf31a70668b1043f502a439
Mehwish570-coder <mehwishnazim2083@gmail.com>
2025-09-27 15:16:33 +0000

    variables in javascript

--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -1,1 +1,16 @@
-let name = "harry"
+const accountId = 12345
+let accountEmail ="misha@gmail.com"
+var accountPassword= "20830"
+accountCity = "RWP"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+//accountId = 2 //not allowed
+ accountEmail = "hc@gmail.com"
+ accountPassword = "23455"
+ accountCity = "isl"
+
+
+
+
+console.log(accountId)
